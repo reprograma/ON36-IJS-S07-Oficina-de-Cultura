004cc8b2507c405402b898062bf8c04aa00876b7
Jô Corrêa <jo.correa@thoughtworks.com>
2024-08-19 22:38:50 -0300

    Adiciona validacao de dados

--- exercicios/para-sala/sistema_oficinas_cultura/src/aluno/dto/create-aluno.dto.ts
+++ exercicios/para-sala/sistema_oficinas_cultura/src/aluno/dto/create-aluno.dto.ts
@@ -1,7 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
 export class CreateAlunoDto {
+  @IsString()
   nome: string;
+
+  @IsString()
   endereco: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
   telefone: string;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
   anoNascimento: number;
 }
--- exercicios/para-sala/sistema_oficinas_cultura/src/main.ts
+++ exercicios/para-sala/sistema_oficinas_cultura/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: false, // Retorna 404 se o payload não atender as regras do DTO
+      whitelist: true, // Remove campos que não estão no DTO
+      transform: true, // Transforma os campos para o tipo especificado no DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
