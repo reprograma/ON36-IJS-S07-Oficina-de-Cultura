caee5447932a058fe452e0c917f0c5b806247c83
Jô Corrêa <jo.correa@thoughtworks.com>
2024-08-19 23:52:04 -0300

    Define entidade Aluno

--- exercicios/para-sala/sistema_oficinas_cultura/src/aluno/entities/aluno.entity.ts
+++ exercicios/para-sala/sistema_oficinas_cultura/src/aluno/entities/aluno.entity.ts
@@ -1,1 +1,16 @@
-export class Aluno {}
+export class Aluno {
+  id: string;
+  nome: string;
+  endereco: string;
+  email: string;
+  telefone: string;
+  cursos: string[];
+
+  constructor(nome: string, endereco: string, email: string, telefone: string) {
+    this.nome = nome;
+    this.endereco = endereco;
+    this.email = email;
+    this.telefone = telefone;
+    this.cursos = [];
+  }
+}
