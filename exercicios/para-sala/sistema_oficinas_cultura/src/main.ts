b3d1115365fcd76c23b5c22f80ad54d0a5264ee3
Jô Corrêa <jo.correa@thoughtworks.com>
2024-08-21 20:24:29 -0300

    Altera main para definir o driver a ser utilizado

--- exercicios/para-sala/sistema_oficinas_cultura/src/main.ts
+++ exercicios/para-sala/sistema_oficinas_cultura/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule.register({
+      driver: 'in-memory', //Ou 'in-file'
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: false, // Retorna 404 se o payload não atender as regras do DTO
