0d5cf88d923f65d66c0252cafe126ca31012c00f
Jô Corrêa <jo.correa@thoughtworks.com>
2024-08-21 20:08:36 -0300

    Conecta o modulo aluno ao modulo de infraestrutura

--- exercicios/para-sala/sistema_oficinas_cultura/src/aluno/application/aluno.module.ts
+++ exercicios/para-sala/sistema_oficinas_cultura/src/aluno/application/aluno.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Type, DynamicModule } from '@nestjs/common';
 import { AlunoController } from '../presenters/http/aluno.controller';
 import { AlunoService } from './aluno.service';
 import { AlunoFactory } from '../domain/factories/aluno-factory';
@@ -7,4 +7,16 @@ import { AlunoFactory } from '../domain/factories/aluno-factory';
   controllers: [AlunoController],
   providers: [AlunoService, AlunoFactory],
 })
-export class AlunoModule {}
+export class AlunoModule {
+  // Aqui criamos um método estático que nos permite escolher qual módulo de persistência queremos usar.
+  static comInfraestrutura(infrastructureModule: Type | DynamicModule) {
+    return {
+      module: AlunoModule,
+      imports: [infrastructureModule],
+    };
+  }
+}
+
+// Aqui estamos usando o padrão de composição de módulos do NestJS. Que permite que os consumidores do módulo
+// escolham qual módulo de persistência querem usar. Isso é um padrão muito poderoso, que nos permite desacoplar
+// a infraestrutura da camada da aplicação.
